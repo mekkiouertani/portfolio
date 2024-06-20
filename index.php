ccbda0333b5498b0b9f487e07992d66681d70c13
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-06-20 10:14:22 +0200

    Update index.php and send-email.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo "Hello, Heroku!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+</head>
+
+<body>
+    <h1>Contact Form</h1>
+    <form action="php/send-email.php" method="post">
+        <label for="name">Nome:</label>
+        <input type="text" id="name" name="name" required>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="message">Messaggio:</label>
+        <textarea id="message" name="message" cols="30" rows="7" required></textarea>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+
+</html>
